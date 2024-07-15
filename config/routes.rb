4ed6f52a1fbab7868c6e52753cc67d32e4d0e17a
MAEDA AYUMU <>
2024-07-15 15:19:55 +0900

    show comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'articles#index'
-  resources :articles
   devise_for :users
-  delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session_path
+
+  root to: 'articles#index'
+
+  resources :articles do
+    resources :comments,only: [:new, :create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_22_231754) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_15_013720) do
   create_table "articles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "title", null: false
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_22_231754) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
   create_table "users", force: :cascade do |t|
