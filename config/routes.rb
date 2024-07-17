fbd495bab5bf11d78c2d0793bc95d948e78ef547
MAEDA AYUMU <>
2024-07-17 23:02:06 +0900

    no message

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resources :articles do
-    resources :comments,only: [:new, :create]
+    resources :comments, only: [:new, :create]
+
+    resource :like, only: [:create, :destroy]
   end
 
   resource :profile, only: [:show, :edit, :update]
+  resources :favorites, ony: [:index]
 end
