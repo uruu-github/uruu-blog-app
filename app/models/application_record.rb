4a8eb0b3bee264404fcd74f258829aff5b424c0d
MAEDA AYUMU <>
2024-07-30 21:26:13 +0900

    ヘロークの設定

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
+# class ApplicationRecord < ActiveRecord::Base
+#   primary_abstract_class
+# end
+
+#こっちに修正 20240728
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  self.abstract_class = true
 end
