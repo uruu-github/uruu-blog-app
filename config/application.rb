e0042b36f7c987b6d495533bd55c5aeeb414359b
MAEDA AYUMU <>
2024-07-30 21:27:24 +0900

    Revert "Revert "ヘロークの設定""
    This reverts commit e56bcd7bc2d43b08ab07e63e9b482cebaad1c0d8.

--- config/application.rb
+++ config/application.rb
@@ -9,15 +9,18 @@ Bundler.require(*Rails.groups)
 module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 6.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
   end
 end
